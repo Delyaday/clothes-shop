9fe7a00bc1dcb6f8c5723b49269647e6459c3ef3
delya <afsadretdinova@asuproject.ru>
2023-03-17 16:44:45 +0200

    first commit

---
+++ src/DataLayer/DataLayerExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public static class DataLayerExtensions
+    {
+        public static IServiceCollection AddDataLayer(this IServiceCollection services)
+        {
+            services.AddDbContext<DataBase>(ServiceLifetime.Scoped);
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseDataLayer(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var db = services.GetRequiredService<DataBase>();
+                    db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                    loggerFactory.CreateLogger("DataLayer").LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+
+            return app;
+        }
+    }
+}
